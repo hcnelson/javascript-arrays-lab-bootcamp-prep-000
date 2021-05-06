ffc04c9fb7af3554f97230d230e1e562d4e7780c
Helen Nelson <hnelson@fandm.edu>
2021-05-06 02:29:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten(name) {
-  kittens.unshift(name)
+  kittens.push(name)
   return kittens
 }
 function destructivelyPrependKitten(name) {
-  
+  kittens.unshift(name)
 }
 function destructivelyRemoveLastKitten() {
-  
+  kittens.pop()
 }
 function destructivelyRemoveFirstKitten() {
-  
+  kittens.shift()
 }
 function appendKitten(name) {
   
